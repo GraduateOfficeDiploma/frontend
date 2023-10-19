c931ec9b99715db46311706a1a3518d07a45fbdc
nerance <nerow.services@gmail.com>
2023-10-20 00:09:56 +0300

    feat: resolved nextconfig issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['react-email-validator'],
+  pagesDir: 'pages',
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
     BASE_URL: process.env.BASE_URL,
